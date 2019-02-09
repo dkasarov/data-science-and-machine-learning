891b2d949dd9ee966f0e754ff2a66e1e54223a8c
Denis <31469124+dkasarov@users.noreply.github.com>
2019-02-09 13:47:14 +0200

    NumPy operations.

---
+++ NumPy/operations.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+arr = np.arange(0, 11)
+
+hundred = 1 / arr # Also * - + and so on
+
+for i in arr:
+    print(i)
+
+print(np.sqrt(arr))
+print(np.exp(arr))
+print(np.exp2(arr))
+print(np.min(arr))
+print(arr.max())
+print(np.sin(arr))
